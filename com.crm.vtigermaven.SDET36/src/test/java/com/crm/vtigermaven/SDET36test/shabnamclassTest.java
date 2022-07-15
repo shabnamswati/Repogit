c0e780fd4c2e7c1fdd3d219440bbd9f9ab965076
SHABNAM SWATI <SHABNAM SWATI@LAPTOP-PV5IV72T>
2022-07-15 19:03:01 +0530

    added shabnam class

---
+++ com.crm.vtigermaven.SDET36/src/test/java/com/crm/vtigermaven/SDET36test/shabnamclassTest.java
@@ -0,0 +1,12 @@
+package com.crm.vtigermaven.SDET36test;
+
+import org.testng.annotations.Test;
+
+public class shabnamclassTest {
+	@Test
+	public void mymethod1()
+	{
+		System.out.println("i am adding my own method ");
+	}
+
+}
